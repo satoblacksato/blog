2d366642a7b2188fc04c217358ebbd29bd81783a
ernesto.liberio@gmail.com <ernesto.liberio@gmail.com>
2017-09-18 21:18:14 +0200

    Primera clase

---
+++ app/Core/Entities/Category.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Core\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+class Category extends Model
+{
+    use SoftDeletes;
+    protected $table='categories';
+    // protected $connection='mysql';
+    protected $fillable=['name','description'];
+    public function books(){
+        return $this->hasMany(Book::class,'category_id');
+    }
+}
